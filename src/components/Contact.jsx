c871a32a5b237d91f201e05417c77a6a92b1e0b9
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-12-15 14:00:52 +0600

    exprience text updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen pt-16 bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full h-screen pt-20 py-40  bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
